e588a6dc393229d2439cf7d739ccbee93486cf2c
swetarajak <swetarajak001@gmail.com>
2025-04-14 00:00:59 +0530

    allowing multiple guesses with looping and quitting after correct gues

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,31 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number");
 
-    let secret_number = rand::rng().gen_range(1..=100);
+    let secret_number = rand::rng().random_range(1..=100);
 
     // println!("The secret numbet is : {secret_number}");
-    println!("Enter your guess:");
-    let mut guess = String::new();      // mut -> guess variable is mutable 
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Enter your guess:");
+        let mut guess = String::new(); // mut -> guess variable is mutable 
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("Your guess : {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Small!"),
-        Ordering::Greater => println!("Big!"),
-        Ordering::Equal => println!("You Win!!"),
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("Your guess : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Small!"),
+            Ordering::Greater => println!("Big!"),
+            Ordering::Equal => {
+                println!("You Win!!");
+                break;
+            }
+        }
     }
 }
